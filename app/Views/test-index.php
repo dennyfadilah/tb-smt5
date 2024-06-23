e77e85834a9f469c8e0e0f4cf604deb6813b8c48
411212034-SyawalA <411212034@mahasiswa.undira.ac.id>
2024-06-23 14:59:08 +0700

    testing-brancing

---
+++ app/Views/test-index.php
@@ -0,0 +1,21 @@
+<h2>Test Index Doang</h2>
+
+<div class="containment">
+    <div class="row">
+        <div class="col-md-4">
+            <h4>
+                Test 1
+            </h4>
+        </div>
+        <div class="col-md-4">
+            <h4>
+                Test 2
+            </h4>
+        </div>
+        <div class="col-md-4">
+            <h4>
+                Test 3
+            </h4>
+        </div>
+    </div>
+</div>
