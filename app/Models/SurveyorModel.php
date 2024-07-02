ae60b1317c478c3f4c9d79a70b404cc1dd01fd34
411212034-SyawalA <411212034@mahasiswa.undira.ac.id>
2024-07-02 22:09:42 +0700

    Nambahin 4 Models

---
+++ app/Models/SurveyorModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SurveyorModel extends Model
+{
+    protected $table            = 'surveyors';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ["marketing_nama", "komoditas_id", "lokasi_id", "repeat_order", "hasil_survery"];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    // Script
+    function getRepeatOrder($thisID){
+        $repeatOrder = $this->where(["id" => $thisID])->findColumn("repeat_order");
+        $repeatOrder = ($repeatOrder)? "Iya": "Tidak";
+        return $repeatOrder;
+    }
+
+}
