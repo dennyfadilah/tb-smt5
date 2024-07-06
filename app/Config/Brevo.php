04e7678c7fc7f2b4d1c7f5a210bb0be10b8799da
dennyfadilah <dennyfadilah99@gmail.com>
2024-07-06 11:56:54 +0700

    Edit Config Brevo

--- app/Config/Brevo.php
+++ app/Config/Brevo.php
@@ -6,10 +6,10 @@ use CodeIgniter\Config\BaseConfig;
 
 class Brevo extends BaseConfig
 {
-    public $apiKey = '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';
-<<<<<<< HEAD
+    public $apiKey;
+
+    public function __construct()
+    {
+        $this->apiKey = getenv('BREVO_API_KEY');
+    }
 }
-=======
-    // public $apiKey = getenv('BREVO_API_KEY');
-}
->>>>>>> a726341 (Edit Brevo)
